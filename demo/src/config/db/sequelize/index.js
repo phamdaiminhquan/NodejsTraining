cf9d071e8a039d8d88a63013773cd89782314dc6
Quan <pdmquan@gmail.com>
2021-12-31 12:33:25 +0700

    gia lap thanh cong API

--- demo/src/config/db/sequelize/index.js
+++ demo/src/config/db/sequelize/index.js
@@ -1,16 +1,34 @@
 var { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.SEQUELIZE_DATA_NAME, process.env.SEQUELIZE_USER_NAME, process.env.SEQUELIZE_PASSWORD, {
-    host: process.env.SEQUELIZE_HOST,
-    dialect: process.env.SEQUELIZE_DIALECT,
-    omitNull: true
-});
+var db;
 
-try {
-    db.authenticate();
-    console.log('Connection Sequelize successfully.');
-} catch (error) {
-    console.error('Unable to connect Sequelize to the database:', error);
+if(process.env.NODE_ENV){
+    db = new Sequelize(process.env.SEQUELIZE_DATA_NAME, process.env.SEQUELIZE_USER_NAME, process.env.SEQUELIZE_PASSWORD, {
+        host: process.env.SEQUELIZE_HOST,
+        dialect: process.env.SEQUELIZE_DIALECT,
+        omitNull: true
+    });
+    
+    try {
+        db.authenticate();
+        console.log('Connection Sequelize successfully.');
+    } catch (error) {
+        console.error('Unable to connect Sequelize to the database:', error);
+    }
+}else{
+    const envTMA = require('tma-environment/src')
+    db = new Sequelize(envTMA.DATA_NAME, envTMA.USER_NAME, envTMA.PASSWORD, {
+        host: process.env.SEQUELIZE_HOST,
+        dialect: process.env.SEQUELIZE_DIALECT,
+        omitNull: true
+    });
+    
+    try {
+        db.authenticate();
+        console.log('Connection Sequelize successfully.');
+    } catch (error) {
+        console.error('Unable to connect Sequelize to the database:', error);
+    }
 }
 
 module.exports = db;
